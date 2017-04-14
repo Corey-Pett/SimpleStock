f9eccd133256536634463ebe68280ef4a530b86b
Corey <coreyallenpett@gmail.com>
2017-04-14 16:34:32 -0500

    Updated models - to CoreData Added VC's - Chart - Search

---
+++ Stock+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Stock+CoreDataClass.swift
+//  SimpleStock
+//
+//  Created by Corey Pett on 4/14/17.
+//  Copyright © 2017 Corey Pett. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Stock: NSManagedObject {
+
+}
